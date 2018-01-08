cfb5b31586c18267faa94e490f68cf86b284ee69
Corey Rome <coreyrome2@gmail.com>
2018-01-08 10:56:22 -0600

    initial commit for restarting project

---
+++ TrashCollectorProject/Models/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollectorProject.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerId { get; set; }
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+        [Display(Name = "Password")]
+        public string UserPassword { get; set; }
+        [Display(Name = "Day of trash pickup")]
+        public int CustomerDay { get; set; }
+        [Display(Name = "Address")]
+        public string StreetName { get; set; }
+        public string City { get; set; }
+        public int Zip { get; set; }
+        [Display(Name = "Day you want your trash picked up")]
+        public string PickupDay { get; set; }
+        public IEnumerable<string> DaysOfTheWeek { get; set; }
+    }
+}

06749a7b976fa147a38a2a13a0947381a0f7107a
Corey Rome <coreyrome2@gmail.com>
2018-01-09 11:33:41 -0600

    added page titles

--- TrashCollectorProject/Models/Employee.cs
+++ TrashCollectorProject/Models/Employee.cs
@@ -19,6 +19,7 @@ namespace TrashCollectorProject.Models
         public string City { get; set; }
         public int Zip { get; set; }
         public IEnumerable<string> DaysOfTheWeek { get; set; }
+        public bool verifyRoute { get; set; }
         
     }
 }
